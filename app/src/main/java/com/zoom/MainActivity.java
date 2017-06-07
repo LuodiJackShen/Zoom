122ffe59fa20b14fb49f87592aa8d040d61cdde6
LuodiJackShen <luodijack@163.com>
2017-06-07 17:51:33 +0800

    一定程度上优化了卡顿问题

--- app/src/main/java/com/zoom/MainActivity.java
+++ app/src/main/java/com/zoom/MainActivity.java
@@ -12,12 +12,18 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
     private static final int ZOOM_WIDTH = 200;
     private static final int ZOOM_HEIGHT = 200;
+
     private View mRootView;
     private PopupWindow mZoomView;
     private LinearLayout mContainerLy;
     private ImageView mDestIv;
+
+    private Bitmap mCacheBitmap;
+    private Bitmap mDisplayBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,15 +51,13 @@ public class MainActivity extends AppCompatActivity {
         int startX = x;
         int startY = y;
 
-        Bitmap src = mRootView.getDrawingCache();//得到drawingCache,实际上就是个bitmap。
-
         //处理边界问题。
-        if (x + ZOOM_WIDTH > src.getWidth()) {
-            startX = src.getWidth() - ZOOM_WIDTH;
+        if (x + ZOOM_WIDTH > mCacheBitmap.getWidth()) {
+            startX = mCacheBitmap.getWidth() - ZOOM_WIDTH;
         }
 
-        if (y + ZOOM_HEIGHT > src.getHeight()) {
-            startY = src.getHeight() - ZOOM_HEIGHT;
+        if (y + ZOOM_HEIGHT > mCacheBitmap.getHeight()) {
+            startY = mCacheBitmap.getHeight() - ZOOM_HEIGHT;
         }
 
         if (startX - ZOOM_WIDTH / 5 <= 0) {
@@ -68,10 +72,11 @@ public class MainActivity extends AppCompatActivity {
             startY = startY - ZOOM_HEIGHT / 2;
         }
 
+        // TODO:这里涉及到在短时间内产生大量的bitmap对象，会引起GC event，有一定几率造成界面的卡顿。
         //获取最终要显示的部分，这部分也是个bitmap。
-        Bitmap bitmap = Bitmap.createBitmap(src, startX, startY, ZOOM_WIDTH, ZOOM_HEIGHT);
+        mDisplayBitmap = Bitmap.createBitmap(mCacheBitmap, startX, startY, ZOOM_WIDTH, ZOOM_HEIGHT);
+        mDestIv.setImageBitmap(mDisplayBitmap);
 
-        mDestIv.setImageBitmap(bitmap);
         int showX = x - ZOOM_WIDTH / 2;
         int showY = (int) (y - ZOOM_HEIGHT * 1.5);
         mZoomView.showAtLocation(mContainerLy,
@@ -86,8 +91,10 @@ public class MainActivity extends AppCompatActivity {
         int y = (int) event.getRawY();
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                //在这里获取到root view 的Cache，而不是每次在showRoom的时候去获取，避免了卡顿问题。
                 mRootView.setDrawingCacheEnabled(true);//先开启drawingCache.
                 mRootView.buildDrawingCache();//建立drawingCache
+                mCacheBitmap = mRootView.getDrawingCache();//得到drawingCache,实际上就是个bitmap。
                 showZoom(x, y);
                 break;
             case MotionEvent.ACTION_MOVE:
@@ -96,6 +103,11 @@ public class MainActivity extends AppCompatActivity {
             case MotionEvent.ACTION_UP:
                 mZoomView.dismiss();
                 mRootView.setDrawingCacheEnabled(false);//关闭drawingCache。
+                mCacheBitmap.recycle();
+                mCacheBitmap = null;
+                mDisplayBitmap.recycle();
+                mDisplayBitmap = null;
+                mRootView.setDrawingCacheEnabled(false);
                 break;
             default:
                 break;
